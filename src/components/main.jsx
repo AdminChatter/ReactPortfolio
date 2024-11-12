da6d46146826a10877a99e6343acb441e38e6190
AdminChatter <xiangzhenwuyu@gmail.com>
2024-11-12 02:45:25 -0500

    finish main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,3 +6,32 @@ import AboutMe from "../pages/AboutMe";
 import Contact from "../pages/Contact";
 import Portfolio from "../pages/Portfolio";
 import Resume from "../pages/Resume";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: App,
+        children: [
+            {
+                index: true,
+                element: <AboutMe />
+            },
+            {
+                path: "/Contact",
+                element: <Contact />
+            },
+            {
+                path: "/Portfolio",
+                element: <Portfolio />
+            },
+            {
+                path: "/Resume",
+                element: <Resume />
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
